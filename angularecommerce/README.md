3821f62f3a05161eff0acccd2723c98277f8f698
mohamed ali <mohamedali.bellili@abshore.com>
2022-04-21 11:48:53 +0100

    html

--- angularecommerce/README.md
+++ angularecommerce/README.md
@@ -1,4 +1,4 @@
-# Ecommerce
+# Angularecommerce
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 12.2.17.
 
---
+++ angularecommerce/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { NavComponent } from './components/shared/nav/nav.component';
+import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
+import { FiltersComponent } from './components/shopping-cart/filters/filters.component';
+import { ProductListComponent } from './components/shopping-cart/product-list/product-list.component';
+import { CartComponent } from './components/shopping-cart/cart/cart.component';
+import { ProductItemComponent } from './components/shopping-cart/product-list/product-item/product-item.component';
+import { CartItemComponent } from './components/shopping-cart/cart/cart-item/cart-item.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    NavComponent,
+    ShoppingCartComponent,
+    FiltersComponent,
+    ProductListComponent,
+    CartComponent,
+    ProductItemComponent,
+    CartItemComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
